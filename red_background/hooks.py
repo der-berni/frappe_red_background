725ac3ca516e5421d3e65ede01b906232fe76b0d
Florian HENRY <florian.henry@open-concept.pro>
2024-07-31 12:34:19 +0200

    chore: set pyproject.toml, .pre-commit-config.yaml, hook.py from new application boilerplate

--- red_background/hooks.py
+++ red_background/hooks.py
@@ -1,5 +1,3 @@
-from . import __version__ as app_version
-
 app_name = "red_background"
 app_title = "Red Background"
 app_publisher = "ALYF GmbH"
@@ -8,20 +6,21 @@ app_icon = "octicon octicon-file-directory"
 app_color = "grey"
 app_email = "hallo@alyf.de"
 app_license = "MIT"
+# required_apps = []
 
 # Includes in <head>
 # ------------------
 
 # include js, css files in header of desk.html
 app_include_css = "/assets/red_background/css/red_background.css"
-# app_include_js = "/assets/red_background/js/red_background.js"
+# app_include_js = "/assets/anothertest/js/anothertest.js"
 
 # include js, css files in header of web template
-# web_include_css = "/assets/red_background/css/red_background.css"
-# web_include_js = "/assets/red_background/js/red_background.js"
+# web_include_css = "/assets/anothertest/css/anothertest.css"
+# web_include_js = "/assets/anothertest/js/anothertest.js"
 
 # include custom scss in every website theme (without file extension ".scss")
-# website_theme_scss = "red_background/public/scss/website"
+# website_theme_scss = "anothertest/public/scss/website"
 
 # include js, css files in header of web form
 # webform_include_js = {"doctype": "public/js/doctype.js"}
@@ -36,6 +35,11 @@ app_include_css = "/assets/red_background/css/red_background.css"
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
 
+# Svg Icons
+# ------------------
+# include app icons in desk
+# app_include_icons = "anothertest/public/icons.svg"
+
 # Home Pages
 # ----------
 
@@ -44,7 +48,7 @@ app_include_css = "/assets/red_background/css/red_background.css"
 
 # website user home page (by Role)
 # role_home_page = {
-#	"Role": "home_page"
+# 	"Role": "home_page"
 # }
 
 # Generators
@@ -53,34 +57,59 @@ app_include_css = "/assets/red_background/css/red_background.css"
 # automatically create page for each record of this doctype
 # website_generators = ["Web Page"]
 
+# Jinja
+# ----------
+
+# add methods and filters to jinja environment
+# jinja = {
+# 	"methods": "anothertest.utils.jinja_methods",
+# 	"filters": "anothertest.utils.jinja_filters"
+# }
+
 # Installation
 # ------------
 
-# before_install = "red_background.install.before_install"
-# after_install = "red_background.install.after_install"
+# before_install = "anothertest.install.before_install"
+# after_install = "anothertest.install.after_install"
 
 # Uninstallation
 # ------------
 
-# before_uninstall = "red_background.uninstall.before_uninstall"
-# after_uninstall = "red_background.uninstall.after_uninstall"
+# before_uninstall = "anothertest.uninstall.before_uninstall"
+# after_uninstall = "anothertest.uninstall.after_uninstall"
+
+# Integration Setup
+# ------------------
+# To set up dependencies/integrations with other apps
+# Name of the app being installed is passed as an argument
+
+# before_app_install = "anothertest.utils.before_app_install"
+# after_app_install = "anothertest.utils.after_app_install"
+
+# Integration Cleanup
+# -------------------
+# To clean up dependencies/integrations with other apps
+# Name of the app being uninstalled is passed as an argument
+
+# before_app_uninstall = "anothertest.utils.before_app_uninstall"
+# after_app_uninstall = "anothertest.utils.after_app_uninstall"
 
 # Desk Notifications
 # ------------------
 # See frappe.core.notifications.get_notification_config
 
-# notification_config = "red_background.notifications.get_notification_config"
+# notification_config = "anothertest.notifications.get_notification_config"
 
 # Permissions
 # -----------
 # Permissions evaluated in scripted ways
 
 # permission_query_conditions = {
-#	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
 # }
 #
 # has_permission = {
-#	"Event": "frappe.desk.doctype.event.event.has_permission",
+# 	"Event": "frappe.desk.doctype.event.event.has_permission",
 # }
 
 # DocType Class
@@ -88,7 +117,7 @@ app_include_css = "/assets/red_background/css/red_background.css"
 # Override standard doctype classes
 
 # override_doctype_class = {
-#	"ToDo": "custom_app.overrides.CustomToDo"
+# 	"ToDo": "custom_app.overrides.CustomToDo"
 # }
 
 # Document Events
@@ -96,57 +125,71 @@ app_include_css = "/assets/red_background/css/red_background.css"
 # Hook on document methods and events
 
 # doc_events = {
-#	"*": {
-#		"on_update": "method",
-#		"on_cancel": "method",
-#		"on_trash": "method"
-#	}
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+# 	}
 # }
 
 # Scheduled Tasks
 # ---------------
 
 # scheduler_events = {
-#	"all": [
-#		"red_background.tasks.all"
-#	],
-#	"daily": [
-#		"red_background.tasks.daily"
-#	],
-#	"hourly": [
-#		"red_background.tasks.hourly"
-#	],
-#	"weekly": [
-#		"red_background.tasks.weekly"
-#	]
-#	"monthly": [
-#		"red_background.tasks.monthly"
-#	]
+# 	"all": [
+# 		"anothertest.tasks.all"
+# 	],
+# 	"daily": [
+# 		"anothertest.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"anothertest.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"anothertest.tasks.weekly"
+# 	],
+# 	"monthly": [
+# 		"anothertest.tasks.monthly"
+# 	],
 # }
 
 # Testing
 # -------
 
-# before_tests = "red_background.install.before_tests"
+# before_tests = "anothertest.install.before_tests"
 
 # Overriding Methods
 # ------------------------------
 #
 # override_whitelisted_methods = {
-#	"frappe.desk.doctype.event.event.get_events": "red_background.event.get_events"
+# 	"frappe.desk.doctype.event.event.get_events": "anothertest.event.get_events"
 # }
 #
 # each overriding function accepts a `data` argument;
 # generated from the base implementation of the doctype dashboard,
 # along with any modifications made in other Frappe apps
 # override_doctype_dashboards = {
-#	"Task": "red_background.task.get_dashboard_data"
+# 	"Task": "anothertest.task.get_dashboard_data"
 # }
 
 # exempt linked doctypes from being automatically cancelled
 #
 # auto_cancel_exempted_doctypes = ["Auto Repeat"]
 
+# Ignore links to specified DocTypes when deleting documents
+# -----------------------------------------------------------
+
+# ignore_links_on_delete = ["Communication", "ToDo"]
+
+# Request Events
+# ----------------
+# before_request = ["anothertest.utils.before_request"]
+# after_request = ["anothertest.utils.after_request"]
+
+# Job Events
+# ----------
+# before_job = ["anothertest.utils.before_job"]
+# after_job = ["anothertest.utils.after_job"]
 
 # User Data Protection
 # --------------------
@@ -176,6 +219,12 @@ app_include_css = "/assets/red_background/css/red_background.css"
 # --------------------------------
 
 # auth_hooks = [
-#	"red_background.auth.validate"
+# 	"anothertest.auth.validate"
 # ]
 
+# Automatically update python controller files with type annotations for this app.
+# export_python_type_annotations = True
+
+# default_log_clearing_doctypes = {
+# 	"Logging DocType Name": 30  # days to retain logs
+# }
